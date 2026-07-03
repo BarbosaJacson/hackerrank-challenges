4220ca070c82a28952b0cbfd2269b40e7ed03400
Jacson Barbosa <barbosajacson.jb@gmail.com>
2026-07-03 11:36:00 -0300

    feat(js): solve hackerRank day 1 - let and const circle calculations

---
+++ javaScript/let-and-const.js
@@ -0,0 +1,22 @@
+'use strict';
+function main(raio) {
+    // Write your code here. Read input using 'readLine()' and print output using 'console.log()'.
+    const PI = Math.PI;
+    const area = PI * (raio** 2);
+    const perimeter = 2 * PI * raio;
+    // Print the area of the circle:
+    console.log(Number(area));
+    
+    // Print the perimeter of the circle:
+    console.log(Number(perimeter));
+
+    try {    
+        // Attempt to redefine the value of constant variable PI
+        PI = 0;
+        // Attempt to print the value of PI
+        console.log(PI);
+    } catch(error) {
+        console.error("You correctly declared 'PI' as a constant.");
+    }
+}
+main(2.6);
